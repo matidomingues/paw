d81d67f91fa83a8a5303899e79368a83997bf68c
mannias <mannias@gmail.com>
2013-09-06 10:05:02 -0300

    Database configuration, More git ignore

---
+++ src/main/java/ar/edu/itba/paw/manager/DatabaseSettings.java
@@ -0,0 +1,9 @@
+package ar.edu.itba.paw.manager;
+
+public class DatabaseSettings {
+
+	public static final String driver = "org.postgresql.Driver";
+	public static final String connectionString = "jdbc:postgresql:///";
+	public static final String username = "";
+	public static final String password = "";
+}
