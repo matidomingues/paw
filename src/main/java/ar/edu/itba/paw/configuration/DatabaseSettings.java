9653f76c44bbf0c4b9e453ff044763dea34d42bb
mannias <mannias@gmail.com>
2013-09-06 10:13:56 -0300

    eSettings

--- src/main/java/ar/edu/itba/paw/configuration/DatabaseSettings.java
+++ src/main/java/ar/edu/itba/paw/configuration/DatabaseSettings.java
@@ -1,9 +1,9 @@
-package ar.edu.itba.paw.manager;
+package ar.edu.itba.paw.configuration;
 
 public class DatabaseSettings {
 
 	public static final String driver = "org.postgresql.Driver";
 	public static final String connectionString = "jdbc:postgresql:///";
 	public static final String username = "";
-	public static final String password = "";
+	public static final String password = "asdas";
 }
