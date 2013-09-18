54497930b6e9a0cf75dd371d8c4075b84949dcd6
Facundo Martinez <fnmartinez88@gmail.com>
2013-09-18 20:04:32 -0300

    Se agregan excepciones a usar

---
+++ src/main/java/ar/edu/itba/paw/utils/exceptions/InvalidHashtagException.java
@@ -0,0 +1,11 @@
+package ar.edu.itba.paw.utils.exceptions;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: facundo
+ * Date: 18/09/13
+ * Time: 19:46
+ * To change this template use File | Settings | File Templates.
+ */
+public class InvalidHashtagException {
+}
---
+++ src/main/java/ar/edu/itba/paw/utils/exceptions/InvalidOperationExcetion.java
@@ -0,0 +1,11 @@
+package ar.edu.itba.paw.utils.exceptions;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: facundo
+ * Date: 18/09/13
+ * Time: 18:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class InvalidOperationExcetion {
+}
