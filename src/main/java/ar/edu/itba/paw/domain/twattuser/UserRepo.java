968c11afb64e5ba2fcde5490168c1decc5499e73
Facundo Martinez <fnmartinez88@gmail.com>
2014-01-19 21:39:36 -0300

    Improves scalability. Mentions resolved in user repo instead of message helper. Presentation layer and logic layer separated.

--- src/main/java/ar/edu/itba/paw/domain/twattuser/UserRepo.java
+++ src/main/java/ar/edu/itba/paw/domain/twattuser/UserRepo.java
@@ -1,11 +1,14 @@
 package ar.edu.itba.paw.domain.twattuser;
 
 
+import ar.edu.itba.paw.domain.twatt.Twatt;
 import ar.edu.itba.paw.utils.exceptions.DuplicatedUserException;
 
 import java.util.List;
 
 public interface UserRepo {
+
+    public static final String MENTION_REGEX = "(?:\\s|\\A|^)[@]([A-Za-z0-9-_]+)";
 	
 	public TwattUser authenticate(String username, String password);
 
@@ -23,4 +26,6 @@ public interface UserRepo {
 	public TwattUser find(int id);
 	
 	public List<TwattUser> getRecomendations(TwattUser user);
+
+    List<TwattUser> resolveMentions(Twatt twatt);
 }
